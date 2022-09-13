850a156646e2f4f8e7a9cced371407fde26ff942
PaitoDl <dili_felipe@hotmail.com>
2022-09-13 17:09:30 -0300

    Correciones esteticas

--- client/src/components/ContainerCountries/ContainerCountries.jsx
+++ client/src/components/ContainerCountries/ContainerCountries.jsx
@@ -64,9 +64,9 @@ export default function ContainerCountries() {
                     <option value="Africa">Africa</option>
                     <option value="Antarctica">Antarctica</option>
                     <option value="Asia">Asia</option>
-                    <option value="Europe">Europe</option>
-                    <option value="North America">North America</option>
-                    <option value="South America">South America</option>
+                    <option value="Europe">Europa</option>
+                    <option value="North America">America del Norte</option>
+                    <option value="South America">America del Sur</option>
                     <option value="Oceania">Oceania</option>
                   </select>                         
                 </div>
